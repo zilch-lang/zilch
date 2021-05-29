4875fda8821ebf98cf954300a02b9a4e73ca6894
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-05-29 12:11:39 +0200

    gzc-parser: create some parse errors

--- lib/gzc-parser/src/Language/Zilch/Syntax/Errors.hs
+++ lib/gzc-parser/src/Language/Zilch/Syntax/Errors.hs
@@ -2,6 +2,7 @@ module Language.Zilch.Syntax.Errors where
 
 import qualified Text.Megaparsec as MP
 import Language.Zilch.Syntax.Internal (Hintable(..))
+import Text.Diagnose (hint)
 
 data LexerError
   = InvalidEscapeSequence Char
@@ -15,3 +16,16 @@ instance MP.ShowErrorComponent LexerError where
 
 instance Hintable LexerError where
   hints _ = []
+
+data ParseError
+  = MissingEnumVariantParameter
+  deriving (Eq, Ord)
+
+instance Show ParseError where
+  show MissingEnumVariantParameter = "missing parameter list for enumeration constructor"
+
+instance MP.ShowErrorComponent ParseError where
+  showErrorComponent = show
+
+instance Hintable ParseError where
+  hints MissingEnumVariantParameter = [hint "You can specify an empty parameter list '()' if your constructor does not take parameters."]
