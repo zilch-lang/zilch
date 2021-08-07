3e903e2e9c141f9255a2c77e35dc2233f42ae70d
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-08-07 17:09:50 +0200

    gzc-tests: fix all tests for the new API

--- test/Common.hs
+++ test/Common.hs
@@ -5,7 +5,6 @@ import Text.Diagnose (Diagnostic, PrettyText, prettyText, (<~<), Position (Posit
 import Text.PrettyPrint.ANSI.Leijen (plain)
 import Data.Text (Text)
 import qualified Data.Text as Text
-import qualified Data.IndentLocated as I
 import qualified Data.Located as L
 
 fails :: Either (Diagnostic [] String Char) b -> Expectation
@@ -24,11 +23,6 @@ left :: Either a b -> a
 left (Left x) = x
 
 infix 5 @-
-(@-) :: a -> (Integer, Integer, Integer, Integer, Integer, String) -> I.Located a
-x @- (bl, bc, el, ec, i, f) = I.ILocated (Position (bl, bc) (el, ec) f) i x
+(@-) :: a -> (Integer, Integer, Integer, Integer, String) -> L.Located a
+x @- (bl, bc, el, ec, f) = x L.:@ Position (bl, bc) (el, ec) f
 {-# INLINE (@-) #-}
-
-infix 5 @+
-(@+) :: a -> (Integer, Integer, Integer, Integer, String) -> L.Located a
-x @+ (bl, bc, el, ec, f) = x L.:@ L.Position (bl, bc) (el, ec) f
-{-# INLINE (@+) #-}
--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -35,7 +35,7 @@ emptyStream = describe "on an empty stream" do
 
 commentStream :: Spec
 commentStream = describe "on a comment stream" do
-  let content = [ L.InlineComment "abcdef" @- (1, 1, 1, 10, 0, specFilename) ]
+  let content = [ L.InlineComment "abcdef" @- (1, 1, 1, 10, specFilename) ]
   let result = runParser content specFilename
   it "successfully runs" do
     succeeds result (specFilename, [r|-- abcdef|])
@@ -44,9 +44,9 @@ commentStream = describe "on a comment stream" do
 
 simpleExportList :: Spec
 simpleExportList = describe "with only an empty export list" do
-  let content = [ L.Export @- (1, 1, 1, 7, 0, specFilename)
-                , L.LParen @- (1, 8, 1, 9, 0, specFilename)
-                , L.RParen @- (1, 9, 1, 10, 0, specFilename) ]
+  let content = [ L.Export @- (1, 1, 1, 7, specFilename)
+                , L.LParen @- (1, 8, 1, 9, specFilename)
+                , L.RParen @- (1, 9, 1, 10, specFilename) ]
   let result = runParser content specFilename
   it "successfully runs" do
     succeeds result (specFilename, [r|export ()|])
@@ -55,29 +55,29 @@ simpleExportList = describe "with only an empty export list" do
 
 missingParentheseInExport :: Spec
 missingParentheseInExport = describe "with a missing ')' in an export list" do
-  let content = [ L.Export @- (1, 1, 1, 7, 0, specFilename)
-                , L.LParen @- (1, 8, 1, 9, 0, specFilename) ]
+  let content = [ L.Export @- (1, 1, 1, 7, specFilename)
+                , L.LParen @- (1, 8, 1, 9, specFilename) ]
   let result = runParser content specFilename
   it "correctly fails on the missing closing parenthese" do
     fails result
 
 simpleImport :: Spec
 simpleImport = describe "on a simple import" do
-  let content = [ L.Import @- (1, 1, 1, 7, 0, specFilename)
-                , L.Identifier "A" @- (1, 8, 1, 9, 0, specFilename) ]
+  let content = [ L.Import @- (1, 1, 1, 7, specFilename)
+                , L.Identifier "A" @- (1, 8, 1, 9, specFilename) ]
   let result = runParser content specFilename
   it "correctly runs" do
     succeeds result (specFilename, [r|import A|])
   it "generates a single unqualified module import" do
-    right result `shouldBe` CST.Module Nothing [ CST.Import False (([], "A") @+ (1, 8, 1, 9, specFilename)) Nothing Nothing @+ (1, 1, 1, 9, specFilename)] []
+    right result `shouldBe` CST.Module Nothing [ CST.Import False (([], "A") @- (1, 8, 1, 9, specFilename)) Nothing Nothing @- (1, 1, 1, 9, specFilename)] []
 
 openedImportNoList :: Spec
 openedImportNoList = describe "on an opened import with no import list" do
-  let content = [ L.Open @- (1, 1, 1, 5, 0, specFilename)
-                , L.Import @- (1, 6, 1, 12, 0, specFilename)
-                , L.Identifier "A" @- (1, 13, 1, 14, 0, specFilename) ]
+  let content = [ L.Open @- (1, 1, 1, 5, specFilename)
+                , L.Import @- (1, 6, 1, 12, specFilename)
+                , L.Identifier "A" @- (1, 13, 1, 14, specFilename) ]
   let result = runParser content specFilename
   it "correctly runs" do
     succeeds result (specFilename, [r|open import A|])
   it "generates an opened import" do
-    right result `shouldBe` CST.Module Nothing [ CST.Import True (([], "A") @+ (1, 13, 1, 14, specFilename)) Nothing Nothing @+ (1, 1, 1, 14, specFilename) ] []
+    right result `shouldBe` CST.Module Nothing [ CST.Import True (([], "A") @- (1, 13, 1, 14, specFilename)) Nothing Nothing @- (1, 1, 1, 14, specFilename) ] []
