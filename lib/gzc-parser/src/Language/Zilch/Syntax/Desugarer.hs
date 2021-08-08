4c1e42d0930bfbf8cf66976b2b98156b7459871c
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-08-08 11:17:03 +0200

    gzc-parser: desugar module meta-information (import, export)

--- lib/gzc-parser/src/Language/Zilch/Syntax/Desugarer.hs
+++ lib/gzc-parser/src/Language/Zilch/Syntax/Desugarer.hs
@@ -1,23 +1,30 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TupleSections #-}
 
 module Language.Zilch.Syntax.Desugarer (runDesugarer) where
 
-import Control.Monad.State.Strict (MonadState, evalStateT, modify)
+import Control.Monad.State.Strict (MonadState, evalStateT, modify, get)
 import Data.HashMap.Strict (HashMap)
 import Data.Text (Text)
 import qualified Language.Zilch.Core.ConcreteSyntaxTree as CST
 import qualified Language.Zilch.Core.AbstractSyntaxTree as AST
 import Text.Diagnose (Diagnostic, (<++>), diagnostic)
 import Control.Monad.Except (MonadError, runExcept, throwError)
-import Language.Zilch.Syntax.Errors (DesugarerError (ConflictingFixitySpecifiers), fromDesugarerError)
+import Language.Zilch.Syntax.Errors (DesugarerError (ConflictingFixitySpecifiers, AliasedOperatorInImport), fromDesugarerError)
 import Data.Bifunctor (first)
 import qualified Data.HashMap.Strict as H
-import Control.Monad (void, forM)
+import Control.Monad (void, forM, when)
 import Data.Located (Located((:@)), unwrapLocated)
 import Data.Maybe (catMaybes)
 import Data.Functor ((<&>))
+import Data.Foldable (foldl')
+import Language.Zilch.Syntax.Internal (qualIdentToModuleName)
+import qualified Data.Text as Text
+import Data.Char (isSymbol, isPunctuation)
+import Debug.Trace (trace, traceShow)
+import Control.Applicative (liftA2)
 
 type Desugarer m = (MonadState DesugarerState m, MonadError DesugarerError m)
 
@@ -59,5 +66,46 @@ exploreModule modName (CST.Module _ _ decls) = do
     extractFixity _                  = undefined
 
 desugarModules :: Desugarer m => [Text] -> HashMap Text CST.Module -> m (HashMap Text AST.Module)
-desugarModules [] _        = pure mempty
-desugarModules (m:ms) mods = desugarModules ms mods
+desugarModules ms mods = foldMapM (\ m -> H.singleton m <$> desugarModule (mods H.! m)) ms
+  where
+    foldMapM :: (Traversable t, Applicative f, Monoid m) => (a -> f m) -> t a -> f m
+    foldMapM f l = foldl' (<>) mempty <$> traverse f l
+
+desugarModule :: Desugarer m => CST.Module -> m AST.Module
+desugarModule (CST.Module exportList imports declarations) = do
+  -- TODO:
+  -- 3. desugar all top-level bindings
+
+  (modImports, operatorFixities) <- unzip <$> forM imports \ (CST.Import open m@(modName :@ p1) qual importedDefs :@ p) -> do
+    DesugarerState st <- get
+    let modN = qualIdentToModuleName modName :@ p1
+
+    ops <- case importedDefs of
+      Nothing -> pure mempty
+      Just ds -> H.fromList <$> flip mapMaybeM ds \ (CST.ImportList _(d :@ p) a :@ _) -> do
+        case a of
+          Nothing        -> pure ()
+          Just (op :@ _) -> when (isOperator d && isOperator op) do
+            throwError (AliasedOperatorInImport (qualIdentToModuleName d) (qualIdentToModuleName op) p)
+
+        pure $ (d, ) <$> st H.! unwrapLocated modN H.!? d
+
+    pure (AST.Import open m qual (desugarImportList <$> importedDefs) {- TODO -} :@ p, ops)
+
+  let modHeader = AST.ModHead (desugarExportList <$> exportList) modImports
+
+  pure (AST.Module modHeader mempty)
+  where
+    mapMaybeM f l = catMaybes <$> traverse f l
+    {-# INLINE mapMaybeM #-}
+
+    desugarExportList = fmap \ (CST.Export iety name :@ _) -> (desugarIEType <$> iety, name)
+
+    desugarImportList = fmap \ (CST.ImportList iety modN alias :@ _) -> (desugarIEType <$> iety, modN, alias)
+
+    desugarIEType CST.ModuleIE = AST.ModuleIE
+    desugarIEType CST.TypeIE   = AST.TypeIE
+    desugarIEType CST.EffectIE = AST.EffectIE
+
+isOperator :: CST.Identifier -> Bool
+isOperator (_, ident) = Text.any (liftA2 (||) isSymbol isPunctuation) ident
