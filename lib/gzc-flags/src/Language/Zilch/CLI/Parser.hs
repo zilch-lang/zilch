a39baa3783dde7cf382524dca751a66b89268009
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2022-11-21 10:18:13 +0100

    document flags using a --man command-line option

--- lib/gzc-flags/src/Language/Zilch/CLI/Parser.hs
+++ lib/gzc-flags/src/Language/Zilch/CLI/Parser.hs
@@ -2,23 +2,117 @@
 {-# LANGUAGE BinaryLiterals #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE NoOverloadedLists #-}
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 module Language.Zilch.CLI.Parser (getFlags) where
 
 import Control.Applicative ((<|>))
-import Data.List (nub, partition)
+import Data.List (intercalate, nub, partition)
 import Language.Zilch.CLI.Flags
-import Options.Applicative (Parser, action, argument, completeWith, customExecParser, eitherReader, fullDesc, help, helper, hidden, info, internal, long, many, metavar, option, prefs, short, showDefault, showHelpOnError, str, strOption, switch, value, (<**>))
+import Options.Applicative (ParseError (..), Parser, action, argument, completeWith, customExecParser, eitherReader, fullDesc, help, helper, hidden, info, internal, long, many, metavar, noArgError, option, prefs, readerAbort, short, showDefault, showHelpOnError, str, strOption, switch, value, (<**>))
+import Options.Applicative.Types (readerAsk)
 import Prelude hiding (all)
 
 getFlags :: IO Flags
 getFlags = customExecParser preferences opts
   where
-    opts = info (pCli <**> helper) (fullDesc)
+    opts = info (pCli <**> helper <**> man) (fullDesc)
     preferences = prefs showHelpOnError
 
 ---------------------------------------------------------------------------------
+
+man :: Parser (a -> a)
+man =
+  option manReader $
+    mconcat
+      [ long "man",
+        metavar "[FLAG]",
+        noArgError (InfoMsg fullMan),
+        help "Show help about the command line flag FLAG (such as \"-d\"). FLAG is a comma-separated list of flags. If FLAG is unspecified or empty, show help for all the command line flags",
+        hidden
+      ]
+  where
+    manReader = do
+      flag <- readerAsk
+      readerAbort $ InfoMsg case filter (not . null) $ splitOn ',' flag of
+        [] -> fullMan
+        flags -> intercalate "\n\n" [manEntry f | f <- flags]
+
+    fullMan :: String
+    fullMan =
+      "Manual entries for all flags:\n"
+        <> intercalate "\n\n" [entryText f | f <- allFlags]
+
+    manEntry flag
+      | flag `elem` allFlags = entryHeader flag <> "\n" <> entryText flag
+      | otherwise = "Option \"" <> flag <> "\" unknown. Check --help or --man for a list of supported options."
+
+    entryHeader flag = "Manual entry for flag \"" <> flag <> "\":"
+
+    splitOn _ "" = []
+    splitOn c str = case break (== c) str of
+      (prefix, "") -> [prefix]
+      (prefix, _ : suffix) -> prefix : splitOn c suffix
+
+    entryText :: String -> String
+    entryText "-d" =
+      "  -d FLAG: Turn on various compiler debugging flags.\n\
+      \           These options are meant to be used to create dumps when reporting bugs.\n\
+      \           An exhaustive list is given underneath, along with simple explanations.\n\
+      \\n\
+      \     dump-dir=DIR:\n\
+      \         Set the dumping directory to DIR.\n\
+      \         The default value for DIR is \"./.zilch/dump\".\n\
+      \     dump-ast:\n\
+      \         Dump the pretty-printed AST (after parsing and desugaring) to \"DIR/<hash>-ast.dbg.zc\".\n\
+      \     dump-tast:\n\
+      \         Dump a pretty-printed version of the typed AST, that is the AST with all type information filled.\n\
+      \         This may be a bit unreadable at times.\n\
+      \         The typed AST is dumped to \"DIR/<hash>-tast.dbg.zc\".\n\
+      \     dump-anf:\n\
+      \         Dump the A-normal form of each module to \"DIR/<hash>-anf.dbg.zc\".\n\
+      \     dump-anf-opt:\n\
+      \         Dump the optimized A-normal form where all modules are merged into one.\n\
+      \         This flag will be ignored if \"--no-main\" is specified.\n\
+      \         The optimized A-normal form is dumped to the file \"DIR/<hash>-anf-opt.dbg.zc\"."
+    entryText "-f" =
+      "  -f FLAG: Control various compiler options.\n\
+      \           FLAG is one of the following:\n\
+      \\n\
+      \     [no-]color-diagnostics:\n\
+      \         Turn on or off colors when in the error reports.\n\
+      \         Colors are on by default, but this may not suit some operating systems."
+    entryText "-W" =
+      "  -W [no-]WARN: Turn on/off specific warnings (or most of them).\n\
+      \                To turn on a warning, simply specify \"-W WARN\".\n\
+      \                To turn it off, specify \"-W no-WARN\".\n\
+      \                Negation has priority over anything else, no matter the order, meaning that if both\n\
+      \                \"-W X\" and \"-W no-X\" are specified then the warning \"X\" will not be raised.\n\
+      \                WARN is one of the following:\n\
+      \\n\
+      \     unused-binding:\n\
+      \         Throw a warning when an unrestricted binding has not been used.\n\
+      \         Consider the following example:\n\
+      \             let f() := 25\n\
+      \             public let g() := ()\n\
+      \         \"f\" is not public and has not been used within the module, therefore a warning should be raised.\n\
+      \     rec-non-rec:\n\
+      \         Raise a warning when a binding declared with \"rec\" hasn't been used recursively\n\
+      \         (thus can be transformed into a normal non-recursive binding).\n\
+      \     additive-singleton:\n\
+      \         This warning stems from the fact that \"⟨x⟩\" is strictly equivalent to \"x\".\n\
+      \         This means that the extra pair of angles can be avoided.\n\
+      \         The same does not hold for \"(x)\", hence the name of the warning.\n\
+      \     all:\n\
+      \         This option cannot be negated (there is no \"no-all\" counterpart).\n\
+      \         Turn on all the non-harming warnings, such as \"unused-binding\", \"rec-non-rec\", \"additive-singleton\".\n\
+      \     error:\n\
+      \         Treat all warnings as errors.\n\
+      \         If this is unwanted, pass \"-W no-error\" to the command-line."
+    entryText _ = "Ohhhhh, you found me! Great job. :) But this shouldn't happen..."
+
+    allFlags = ["-d", "-f", "-W"]
 
 pCli :: Parser Flags
 pCli = do
