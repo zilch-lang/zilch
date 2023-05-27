9490de0aa0652375f6bf0aecd97d344d1eae4dbf
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2023-05-27 17:47:56 +0200

    provide compatibility for non-flake users

--- nix/nixpkgs-pinned.nix
+++
@@ -1,9 +0,0 @@
-import
-  (builtins.fetchTarball {
-    name = "nixpkgs-pinned";
-    url = "https://github.com/nixos/nixpkgs/archive/ba6ba2b90096dc49f448aa4d4d783b5081b1cc87.tar.gz";
-    # Use `nix-prefetch-url --unpack <url>`
-    sha256 = "07asnnvp49wg419f7819iv7y8xhgxxzig1fjm72ii1g2mi3vmc1s";
-  })
-{ }
-
